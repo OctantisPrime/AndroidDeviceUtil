d0954d1c7116ed255fe3105a17c7fe59207df811
Csx <csx_mail@163.com>
2024-03-12 17:46:49 +0800

    Initial commit

---
+++ app/src/main/java/com/octantis/prime/android/device/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.octantis.prime.android.device.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.octantis.prime.android.deviceutils.DeviceMain
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
